923e375cbbae7e5cd4d905fa87f094397b0d63d3
kami <danielwaechtler@protonmail.com>
2023-08-23 18:36:07 +0200

    check if channel is added, and add more models and properties

--- CallbackServerPromoCodes/Models/Video.cs
+++ CallbackServerPromoCodes/Models/Video.cs
@@ -1,22 +1,34 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace CallbackServerPromoCodes.Models;
 
-[Index(nameof(VideoId))]
+[Index(nameof(Id))]
 public class Video
 {
-    public Video(string videoId, string channelId)
+    public Video()
     {
-        VideoId = videoId;
-        ChannelId = channelId;
-        Processed = false;
     }
-    
-    [Key]
-    public string VideoId { get; set; }
 
-    public string ChannelId { get; set; }
+    public Video(string videoId, string link, Channel channel)
+    {
+        Id = videoId;
+        Channel = channel;
+        Link = link;
+        Processed = false;
+        Promotions = new List<Promotion>();
+    }
+
+    public string Id { get; set; }
 
     public bool Processed { get; set; }
+
+    public string Link { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Title { get; set; }
+
+    public List<Promotion> Promotions { get; set; }
+
+    public Channel Channel { get; set; }
 }
